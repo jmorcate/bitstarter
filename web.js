e73514bd276bb770c0a801d7e871ce8cb75c620f
Joaquin Morcate <jmorcate@gmail.com>
2013-07-09 21:51:29 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buffer = new Buffer(256);
+  buffer = fs.readFileSync(infile);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
